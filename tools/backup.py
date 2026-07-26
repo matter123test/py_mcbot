156cb9686a110bb0202f96bbffabbf6809d3090a
matter123test <84776941+matter123test@users.noreply.github.com>
2026-07-26 13:52:13 +0200

    Better tooling

--- tools/backup.py
+++ tools/backup.py
@@ -6,32 +6,63 @@ from rich.console import Console
 
 console = Console()
 
-WORLD_FOLDER = 'server/world'
-BACKUPS_FOLDER = 'backups'
-
-
-def make_backup(archive_name):
-    if not os.path.exists(BACKUPS_FOLDER):
-        console.print("[bold yellow]Created backups folder")
-        os.mkdir(BACKUPS_FOLDER)
-
-    shutil.make_archive(archive_name, 'zip', WORLD_FOLDER)
+from argparse import ArgumentParser
 
 
 def main():
+    parser = ArgumentParser(
+        prog="Backup tool", description="Creates backups from world folder"
+    )
+
+    parser.add_argument("-n", "--name", help="name of the output file", default=None)
+    parser.add_argument(
+        "-f",
+        "--backup_folder",
+        help="specify the output backups folder",
+        default="backups",
+    )
+    parser.add_argument(
+        "-w",
+        "--world-folder",
+        help="specify where the world folder is located",
+        default="server/world",
+    )
+
+    args = parser.parse_args()
+
+    archive_name = args.name
+    if archive_name is None:
+        archive_name = get_archive_name(args.backup_folder)
+    else:
+        archive_name = os.path.join(args.backup_folder, archive_name)
+
+    with console.status("[bold yellow]Creating backup..."):
+        make_backup(
+            archive_name=archive_name,
+            backups_folder=args.backup_folder,
+            world_folder=args.world_folder,
+        )
+
+    console.print(f"[bold green]Created backup at {archive_name}")
+
+
+def get_archive_name(backups_folder: str) -> str:
     now = datetime.datetime.now()
 
     # Example output: "backup_2026-06-06_17-00"
-    archive_name = now.strftime(f"{BACKUPS_FOLDER}/backup_%Y-%m-%d_%H-%M")
+    return now.strftime(f"{backups_folder}/backup_%Y-%m-%d_%H-%M")
 
-    with console.status("[bold yellow]Creating backup..."):
-        make_backup(archive_name)
 
-    console.print(f"[bold green]Created backup at {archive_name}")
+def make_backup(archive_name: str, backups_folder: str, world_folder: str):
+    if not os.path.exists(backups_folder):
+        console.print("[bold yellow]Created backups folder")
+        os.mkdir(backups_folder)
+
+    shutil.make_archive(archive_name, "zip", world_folder)
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        console.print("[bold red]Backup cancelled!")
+        console.print("[bold red]Backup interrupted!")
